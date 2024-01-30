d7064ce9e601a046b3a6db52c482d1bb266d9ace
vivian Birungi <viviangal20@gmail.com>
2024-01-30 22:30:03 +0300

    added baseurl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV = 'production';
 const nextConfig = {
+  basePath: isProd ? '/rentout_admin/dashboard/property':'',
    output: "export",
+   images:{
+    unoptimized:true
+   }
    
  
   
