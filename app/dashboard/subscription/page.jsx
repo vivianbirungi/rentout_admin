39c9615fa3bfbfdb8f80e438ae341808ef3ae7d1
katende Hakim <ghkatende@gmail.com>
2024-02-02 20:23:59 +0000

    Added the subscription

--- app/dashboard/subscription/page.jsx
+++ app/dashboard/subscription/page.jsx
@@ -1,102 +1,58 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Pagination from '../../ui/dashboard/pagination/pagination';
-import Search from '../../ui/dashboard/search/search';
-import styles from '../../ui/dashboard/users/users.module.css';
-import { MdDelete, MdRemoveRedEye } from 'react-icons/md';
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Pagination from "../../ui/dashboard/pagination/pagination";
+import Search from "../../ui/dashboard/search/search";
+import styles from "../../ui/dashboard/users/users.module.css";
+import { MdDelete, MdRemoveRedEye } from "react-icons/md";
+import useRLStore from "../../lib/store";
 
 const SubscriptionPage = () => {
+  const { getSubscriptions, subscriptions } = useRLStore((state) => state);
+
+  console.log(subscriptions);
+
+  useEffect(() => {
+    getSubscriptions();
+  }, []);
+
   return (
     <div className={styles.container}>
-    <div className={styles.top}>
-      <Search placeholder="Search for a subscription..." />
-      <Link href="/dashboard/users/add">
-        <button className={styles.addButton}>Add New</button>
-      </Link>
-    </div>
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <td >Name</td>
-          <td className='hidden'>Email</td>
-          <td className='hidden'>Created At</td>
-          <td className='hidden'>Role</td>
-          <td className='hidden'>Status</td>
-          <td >Action</td>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-              <td>
-                <div className={styles.user}>
-                  <Image
-                    src="/noavatar.png"
-                    alt=""
-                    width={40}
-                    height={40}
-                    className={styles.userImage}
-                  />
-                  John Doe
-                </div>
-              </td>
-              <td className='hidden'>viviangal@email.com</td>
-              <td className='hidden'>12.02.2023</td>
-              <td className='hidden'>Admin</td>
-              <td className='hidden'>active</td>
-              <td>
-              <div className={styles.buttons}>
-                  <Link href={`/dashboard/users/`}>
-                    <button className={`${styles.button} ${styles.view}`}>
-                     <MdRemoveRedEye/>
-                    </button>
-                  </Link>
-                  
-                    <input type="hidden" name="id" value='' />
-                    <button className={`${styles.button} ${styles.delete}`}>
-                      <MdDelete/>
-                    </button>
-                 
-                </div>
-              </td>
-      </tr>
-      <tr>
-              <td>
-                <div className={styles.user}>
-                  <Image
-                    src="/noavatar.png"
-                    alt=""
-                    width={40}
-                    height={40}
-                    className={styles.userImage}
-                  />
-                  John Doe
-                </div>
-              </td>
-              <td className='hidden'>viviangal@email.com</td>
-              <td className='hidden'>12.02.2023</td>
-              <td className='hidden'>Admin</td>
-              <td className='hidden'>active</td>
-              <td>
-                <div className={styles.buttons}>
-                  <Link href={`/dashboard/users/`}>
-                    <button className={`${styles.button} ${styles.view}`}>
-                     <MdRemoveRedEye/>
-                    </button>
-                  </Link>
-                  
-                    <input type="hidden" name="id" value='' />
-                    <button className={`${styles.button} ${styles.delete}`}>
-                      <MdDelete/>
-                    </button>
-                 
-                </div>
-              </td>
-      </tr>
+      <div className={styles.top}>
+        <Search
+          placeholder="Search for a subscription..."
+          onSearch={() => null}
+        />
+      </div>
+      <br />
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td className="hidden bold">Property</td>
+            <td className="hidden">Txt Ref</td>
+            <td className="hidden">Tenants</td>
+            <td className="hidden">Amount</td>
+            <td className="hidden">Payment Status</td>
+            <td className="hidden">Date paid</td>
+          </tr>
+        </thead>
+        <tbody>
+          {subscriptions?.map((subscription) => (
+            <tr>
+              <td className="hidden">{subscription.pro_title}</td>
+              <td className="hidden">{subscription.tx_ref}</td>
+              <td className="hidden">{subscription.total_tenants}</td>
+              <td className="hidden">{subscription.amount}</td>
+              <td className="hidden">{subscription.payment_status}</td>
+              <td className="hidden">{subscription.date_paid}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
     </div>
-  )
-}
+  );
+};
 
-export default SubscriptionPage
+export default SubscriptionPage;
--- app/lib/store.js
+++ app/lib/store.js
@@ -2,43 +2,61 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import instance from '../../config';
 
-const useRLStore = create(persist((set,get) => ({
-        properties: [],
-        tenants: [],
-        landLords: [],
-        subscriptions: [],
-        activeUser: null,
-        activeProperty:null,
-        getProperties: async() => {
-            const response =  await instance.get('get_Properties');
-            // const results = useHttpGet('get_Properties');
-            set((state) => ({ ...state, properties: response.data.results}))},
-        getTenants: async() =>  {
-            const results = await instance.get(`get_Users/tenant`);
-            set((state) => ({ ...state, tenants: results}))},
-        getLandlords: async() =>  {
-                const results = await instance.get(`get_Users/landlord`);
-                set((state) => ({ ...state, landLords: results}))},
-        getSubscriptions: async() =>  {
-            const results = await instance.get('get_Properties');
-            set((state) => ({ ...state, properties: results}))},
-        setActiveUser: (userId, userType)=>  {
-            let user = null;
-            if(userType === 'tenant'){
-                user = get().tenants.find(tenant => tenant.userId === userId);
-            }
-            else{
-                user = get().landLords.find(landLord => landLord.userId === userId);
+const useRLStore = create(
+  persist(
+    (set, get) => ({
+      properties: [],
+      tenants: [],
+      landLords: [],
+      subscriptions: [],
+      activeUser: null,
+      activeProperty: null,
 
-            }
-        
-            set((state) => ({ ...state, activeUser: user}))},
-        setActiveProperty: (propertyId) => {
-            let property = get().properties.find(property => property.prop_id === propertyId);
-            set((state)=>({ ...state, activeProperty: property}))},
+      getProperties: async () => {
+        const response = await instance.get('get_Properties');
+        set((state) => ({ ...state, properties: response.data.results }));
+      },
+
+      getTenants: async () => {
+        const results = await instance.get(`get_Users/tenant`);
+        set((state) => ({ ...state, tenants: results }));
+      },
+
+      getLandlords: async () => {
+        const results = await instance.get(`get_Users/landlord`);
+        set((state) => ({ ...state, landLords: results }));
+      },
+
+      getSubscriptions: async () => {
+        const results = await instance.get('get_subscriptions');
+        console.log(results)
+        set((state) => ({ ...state, subscriptions: results.data.results }));
+      },
+
+      setActiveUser: (userId, userType) => {
+        let user = null;
+
+        if (userType === 'tenant') {
+          user = get().tenants.find((tenant) => tenant.userId === userId);
+        } else {
+          user = get().landLords.find((landLord) => landLord.userId === userId);
+        }
+
+        set((state) => ({ ...state, activeUser: user }));
+      },
+
+      setActiveProperty: (propertyId) => {
+        let property = get().properties.find(
+          (property) => property.prop_id === propertyId
+        );
+        set((state) => ({ ...state, activeProperty: property }));
+      },
     }),
     {
-        name:'rentalynk',
-        storage: createJSONStorage(()=> localStorage)
-    }))
+      name: 'rentalynk',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+
 export default useRLStore;
