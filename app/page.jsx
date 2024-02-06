e58a7c48d6c88b6706ef235b4f9a02179bd2e053
vivian Birungi <viviangal20@gmail.com>
2024-02-06 21:09:37 +0300

    added redirect

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
+"use client"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/dashboard');
+  })
   return (
     <div>Hello</div>
   )
